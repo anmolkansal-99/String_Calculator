53f409b5707b8a11b35bd781833a4ca655da4e1b
anmolkansal246 <anmol.k@binmile.com>
2025-08-11 16:44:23 +0530

    Setup Jest testing "Setup Jest testing Library and configure it

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    // Optional: if you're using Jest-style matchers
+    coverage: {
+      provider: 'istanbul' // or 'c8'
+    }
+  }
 })
